f44fb3f73e1a6720c4d11aa9eafca281d56eea6a
Brian Shewan <w0111036@nscc.ca>
2023-10-04 10:16:55 -0300

    added boolean examples

---
+++ Decisions and Loops/roman.ps1
@@ -0,0 +1,42 @@
+[int]$Number = Read-Host "Enter a number between 1-10"
+
+# if the number is 1, output I
+if ($Number -eq 1) {
+    Write-Host "I"
+}
+# if the number is 2, output II
+if ($Number -eq 2) {
+    Write-Host "II"
+}
+# if the number is 3, output III
+if ($Number -eq 3) {
+    Write-Host "III"
+}
+# if the number is 4, output IV
+if ($Number -eq 4) {
+    Write-Host "IV"
+}
+# if the number is 5, output V
+if ($Number -eq 5) {
+    Write-Host "V"
+}
+# if the number is 6, output VI
+if ($Number -eq 6) {
+    Write-Host "VI"
+}
+# if the number is 7, output VII
+if ($Number -eq 7) {
+    Write-Host "VII"
+}
+# if the number is 8, output VIII
+if ($Number -eq 8) {
+    Write-Host "VIII"
+}
+# if the number is 9, output IX
+if ($Number -eq 9) {
+    Write-Host "IX"
+}
+# if the number is 10, output X
+if ($Number -eq 10) {
+    Write-Host "X"
+}
